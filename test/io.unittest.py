d77fe88bd0aaea424a4517a6e7c72c2e28c7955b
Yipeng Sun <syp@umd.edu>
2018-05-24 21:37:29 -0400

    Preliminary test for excel reading.

--- test/io.unittest.py
+++ test/io.unittest.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 #
 # License: MIT
-# Last Change: Thu May 24, 2018 at 02:52 PM -0400
+# Last Change: Thu May 24, 2018 at 03:21 PM -0400
 
 import unittest
-
+from os.path import join
 
 import sys
 sys.path.insert(0, '..')
 
-from utm.io import parse_cell_range
+from utm.io import parse_cell_range, XLReader
+
+xlsm_filename = join('..', 'templates', 'backplaneMapping_pigtailPins_trueType_strictDepopulation_v5.1.xlsm')
 
 
 class ParseCellRangeTester(unittest.TestCase):
@@ -32,6 +34,13 @@ class ParseCellRangeTester(unittest.TestCase):
         self.assertEqual(initial_row, 12)
         self.assertEqual(str(final_col), 'CD')
         self.assertEqual(final_row, 345)
+
+
+class XLReaderTester(unittest.TestCase):
+    def test_read_single_spec(self):
+        reader = XLReader(xlsm_filename, [0, ], 'B5:H6')
+        result = reader.read()
+        self.assertEqual(result[0][0]['ref'], 199)
 
 
 if __name__ == '__main__':
