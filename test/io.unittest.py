1e16864c469e7b9a9e75b7aaf74ed372c40f11dd
Yipeng Sun <syp@umd.edu>
2018-05-25 16:55:40 -0400

    Preliminary test for read with headers provided.

--- test/io.unittest.py
+++ test/io.unittest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # License: MIT
-# Last Change: Thu May 24, 2018 at 11:04 PM -0400
+# Last Change: Fri May 25, 2018 at 04:55 PM -0400
 
 import unittest
 from os.path import join
@@ -11,7 +11,9 @@ sys.path.insert(0, '..')
 
 from utm.io import parse_cell_range, XLReader
 
-xlsm_filename = join('..', 'templates', 'backplaneMapping_pigtailPins_trueType_strictDepopulation_v5.1.xlsm')
+pt_filename = join('..', 'templates', 'backplaneMapping_pigtailPins_trueType_strictDepopulation_v5.1.xlsm')
+brkoutbrd_filename = join('..', 'templates',
+                          'BrkOutBrd_Pin_Assignments_Mar27_2018_PM1.xlsx')
 
 
 class ParseCellRangeTester(unittest.TestCase):
@@ -38,17 +40,24 @@ class ParseCellRangeTester(unittest.TestCase):
 
 class XLReaderTester(unittest.TestCase):
     def test_read_single_spec(self):
-        reader = XLReader(xlsm_filename)
+        reader = XLReader(pt_filename)
         result = reader.read([0, ], 'B5:H6')
         self.assertEqual(result[0][0]['ref'], 199)
 
     def test_read_sort(self):
-        reader = XLReader(xlsm_filename)
+        reader = XLReader(pt_filename)
         result = reader.read([0, ], 'B5:H11',
                              sortby=lambda item: item['SEAM pin'])
         self.assertEqual(result[0][0]['ref'], 228)
         self.assertEqual(result[0][2]['ref'], 207)
         self.assertEqual(result[0][-1]['ref'], 200)
+
+    def test_read_with_headers_single_spec(self):
+        reader = XLReader(brkoutbrd_filename)
+        headers = {'A': 'Conn', 'D': 'Descr'}
+        result = reader.read(['PinAssignments'], 'A4:D18', headers=headers)
+        self.assertEqual(result[0][0]['Conn'], 'JD11_JPL2_1V5_M')
+        self.assertEqual(result[0][-1]['Descr'], 'GND')
 
 
 if __name__ == '__main__':
