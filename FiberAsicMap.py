0591e24f941d8a1ba1c91498955572a805607130
ZishuoYang <yangzs@umd.edu>
2018-10-08 00:25:37 -0400

    Create FiberAsicMap for mapping ASICs to fiber/GBTx frame

---
+++ FiberAsicMap.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+#
+# License: MIT
+# Last Change: Mon Sep 24, 2018 at 01:48 PM -0400
+
+from pathlib import Path
+
+from pyUTM.io import XLReader, write_to_csv
+from pyUTM.selection import RulePD
+from pyUTM.datatype import BrkStr, GenericNetNode
+from pyUTM.datatype import ExcelCell
+from pyUTM.legacy import legacy_csv_line_dcb
+
+input_dir = Path('input')
+output_dir = Path('output')
+
+pt_filename = input_dir / Path(
+    'backplaneMapping_pigtailPins_trueType_strictDepopulation_v5.2.xlsm')
+dcb_filename = input_dir / Path(
+    'backplaneMapping_SEAMPins_trueType_v5.2.xlsm')
+
+pt_result_output_filename = output_dir / Path('AltiumNetlist_PT.csv')
+dcb_result_output_filename = output_dir / Path('AltiumNetlist_DCB.csv')
+
+
+##########################
+# Read info from PigTail #
+##########################
+
+PtReader = XLReader(pt_filename)
+pt_descr = PtReader.read(range(0, 12), 'B5:K405',
+                         sortby=lambda d: d['Signal ID'])
+
+
+######################
+# Read info from DCB #
+######################
+
+DcbReader = XLReader(dcb_filename)
+dcb_descr = DcbReader.read(range(0, 12), 'B5:K405',
+                           sortby=lambda d: d['Signal ID'])
+
+
+#####################################
+# Form list of ASIC channels to map #
+#####################################
+
+# ASIC list from PT
+asic_descr = []
+
+for pt_idx in range(0, len(pt_descr)):
+    single_pt = []
+    for i in pt_descr[pt_idx]:
+        if 'ASIC' in i['Signal ID']:
+            if i['Signal ID'].font_color is not None and \
+                    i['Signal ID'].font_color.rgb == 'FF3366FF':
+                        i['Attr'] = 'DEPOPULATED'
+            elif i['Signal ID'].font_color is not None and \
+                    i['Signal ID'].font_color.tint != 0.0:
+                        i['Attr'] = 'INVALID'
+            else:
+                i['Attr'] = 'REGULAR'
+            single_pt.append(i)
+    asic_descr.append(single_pt)
+
+
+def get_asic_ch(pt_idx, pt_pin):
+    for i in asic_descr[pt_idx]:
+        if pt_pin == str(i['Pigtail pin']):
+            return i
+
+
+# GBTx-ASIC list from DCB
+gbtx_desc = []
+
+for dcb_idx in range(0, len(dcb_descr)):
+    single_dcb = []
+    for i in dcb_descr[dcb_idx]:
+        if '_ELK_CH' in i['Signal ID']:
+            # Find GBTx-ASIC connections
+            if i['Pigtail pin'] is not None:
+                pt_idx = int(i['Pigtail slot'][:2])
+                pt_pin = str(i['Pigtail pin'])
+                asic_ch = get_asic_ch(pt_idx, pt_pin)
+                i['PT Signal ID'] = asic_ch['Signal ID']
+                i['PT Attr'] = asic_ch['Attr']
+            else:
+                i['PT Signal ID'] = None
+                i['PT Attr'] = None
+            single_dcb.append(i)
+    gbtx_desc.append(single_dcb)
+
+# Now do fiber mapping
